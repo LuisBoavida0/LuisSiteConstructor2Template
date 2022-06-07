b6d82e291a4c04d0cfaa39aa8f883f2f98b65537
Luis Boavida <luis.128.b@gmail.com>
2022-06-07 14:31:48 +0100

    Added bootstrap to the project

--- public/server/globalImports.js
+++ public/server/globalImports.js
@@ -3,3 +3,9 @@
 import handlebarsImport from 'handlebars'
 //Handlebars variable is created in helper.js and it is being equalled to handlebarsImport, and now the module is accessible globally
 Handlebars = handlebarsImport 
+
+//If you want to use bootstrap (you need to run npm install bootstrap)
+//import "bootstrap"
+//If you want to use only one part of bootstrap (Only the js part)
+//import "bootstrap/js/dist/modal"
+//import "bootstrap/js/dist/alert"
